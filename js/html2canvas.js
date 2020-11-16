428eb16037cbf0c585d292f95a6fab4a48429987
wangkang123456789 <185720667@qq.com>
2020-11-16 11:03:16 +0800

    ***

--- js/html2canvas.js
+++ js/html2canvas.js
@@ -2534,7 +2534,7 @@
                                     img.onerror = reject;
                                     //ios safari 10.3 taints canvas with data urls unless crossOrigin is set to anonymous
                                     if (isInlineBase64Image(src) || useCORS) {
-                                        img.crossOrigin = 'anonymous';
+                                        img.setAttribute("crossOrigin",'Anonymous')
                                     }
                                     img.src = src;
                                     if (img.complete === true) {
